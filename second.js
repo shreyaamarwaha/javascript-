acd24fe642b3914835bf23e789eb806995b13ecd
shreya marwaha <shreyaamarwaha@gmail.com>
2024-01-21 01:12:22 +0530

    first

---
+++ second.js
@@ -0,0 +1,30 @@
+//this is a single line comment
+
+/*this is a multiline comment 
+hello, yeh hai javascript ka second lesson 
+hello 
+hello
+*/
+
+//talking about operators now,
+
+ let a=4;
+ let b=5;
+
+
+//  console.log("a+b =", a+b);
+//  console.log("a * b =", a*b);
+//  console.log("a / b =", a/b);
+//  console.log("the output of a**b", a**b);
+//  console.log("a % b", a%b);
+//  console.log("a * b", a*b);
+
+//UNARY OPERATORS (VERY IMP FOR LOOPS, ACHHE SE SAMAJH LENA)
+
+a++;
+
+console.log("a=",a);
+z
+
+
+
